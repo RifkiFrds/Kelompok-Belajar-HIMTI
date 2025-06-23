e8791ff84f849cb07700d949821ea44785305efe
muhamadrifkifirdaus22@gmail.com <muhamadrifkifirdaus22@gmail.com>
2025-06-23 14:47:58 +0700

    add pertemuan 4

---
+++ Learning-Group/pertemuan-4/script/data.js
@@ -0,0 +1,28 @@
+const klinikHimti = [
+  {
+    title: "Merchandise HIMTI",
+    description: "Dapatkan Koleksi T-shirt, dan Tote bag dengan kualitas terbaik!",
+    images: "assets/images/himti-merch.jpg",
+    price: 50000,
+    button: "Hubungi Kami"
+  },
+  {
+    title: "Keyboard Gaming",
+    description: "Tersedia Keyboard gaming untuk kebutuhan coding mu!",
+    images: "assets/images/keyboard.jpg",
+    price: 300000,
+    button: "Hubungi Kami"
+  },
+  {
+    title: "Jasa Desain",
+    description: "Butuh desain poster, UI/UX, atau logo? Serahkan pada tim HIMTI!",
+    images: "assets/images/jasa-desain.jpg",
+    button: "Hubungi Kami"
+  },
+  {
+    title: "Jasa Service Laptop",
+    description: "Layanan perbaikan laptop cepat dan terpercaya untuk mahasiswa.",
+    images: "assets/images/jasa-service.jpg",
+    button: "Hubungi Kami"
+  },
+];
--- Learning-Group/pertemuan-4/script/index.js
+++ Learning-Group/pertemuan-4/script/index.js
@@ -1,3 +1,19 @@
-function product() {
-    
-}
+const container = document.getElementById("klinik-container");
+let html = "";
+
+klinikHimti.forEach(item => {
+  html += `
+    <article class="card">
+      <img class="item-images" src="${item.images}" alt="${item.title}" />
+      <h3>${item.title}</h3>
+      <p>${item.description}</p>
+      ${item.price ? 
+        `<a href="https://shopee.co.id/lapakinformatika" class="btn cta">Beli - Rp${item.price.toLocaleString('id-ID')}</a>`
+        : `<a href="https://api.whatsapp.com/send?phone=6285840407463" class="btn cta" target="_blank">Hubungi Kami</a>`
+      }
+    </article>
+  `;
+});
+
+container.innerHTML = html;
+
