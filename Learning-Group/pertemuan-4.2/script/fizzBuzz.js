1e9779172fbc6f17b0a4b6e228a8a23d61796a98
muhamadrifkifirdaus22@gmail.com <muhamadrifkifirdaus22@gmail.com>
2025-06-29 22:45:00 +0700

    add modul latihan pertemuan 4.2

--- Learning-Group/pertemuan-4.2/script/fizzBuzz.js
+++ Learning-Group/pertemuan-4.2/script/fizzBuzz.js
@@ -4,18 +4,18 @@ function fizzBuzz(fizz, buzz, batas) {
     for (let i = 1; i <= batas; i++) {
         // Jika i habis dibagi fizz dan buzz, cetak FizzBuzz
         if (i % fizz === 0 && i % buzz === 0) {
-            console.log(`${i} (FizzBuzz ${fizz}, ${buzz})`);
+            console.log(`${i} FizzBuzz `);
         // Jika i habis dibagi fizz, cetak Fizz
         } else if (i % fizz === 0) {
-            console.log(`${i} (Fizz ${fizz})`);
+            console.log(`${i} Fizz`);
         // Jika i habis dibagi buzz, cetak Buzz
         } else if (i % buzz === 0) {
-            console.log(`${i} (Buzz ${buzz})`);
+            console.log(`${i} Buzz`);
         } else {
             console.log(i);
         }
     }
 }
 
-// Panggil fungsi fizzBuzz dengan parameter 3, 5, dan batas 20
-fizzBuzz(3, 5, 20);
+// Panggil fungsi fizzBuzz dengan parameter 3, 5, dan batas 30
+fizzBuzz(3, 5, 30);
